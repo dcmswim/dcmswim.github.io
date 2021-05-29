46dafadfa7334c1ea3edebd70d4fa20e57eeb41f
David <dcmswim@gmail.com>
2021-05-28 17:21:02 -0700

    push update
    push update for added functionality on portfolio

---
+++ js/portfolio.js
@@ -0,0 +1,37 @@
+//open and close functions for contact form
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+//displays first image in the slideshow when the page loads
+var slideIndex = 1;
+showSlides(slideIndex);
+
+//changes slides when arrows are clicked
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+//changes slide when dot is clicked
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1};
+    if (n < 1) {slideIndex = slides.length};
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
